8c4ad7414b899662771b364bbc02a1a35951f64a
NiButCrazy <3104955349@qq.com>
2026-04-06 10:21:18 +0800

    fix: 不需要immer的部分我给删了

--- src/shared/stores/useGlobalStore.ts
+++ src/shared/stores/useGlobalStore.ts
@@ -1,13 +1,9 @@
 import { create } from 'zustand'
-import { immer } from 'zustand/middleware/immer'
 
 
 export const useGlobalStore = create<GlobalStore>()(
-  immer(set => ({
-      isDark: window.matchMedia('(prefers-color-scheme: dark)').matches,
-      setDark: (isDark) => {
-        set(state => {state.isDark = isDark})
-      }
-    }
-  ))
+  set => ({
+    isDark: window.matchMedia('(prefers-color-scheme: dark)').matches,
+    setDark: (isDark) => set(() => ({ isDark }))
+  })
 )

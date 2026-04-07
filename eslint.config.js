046303d04e4430b9f20fa12619d93abb22b3c27d
NiButCrazy <3104955349@qq.com>
2026-04-07 19:01:49 +0800

    feat: 一堆细节说明

--- eslint.config.js
+++ eslint.config.js
@@ -13,10 +13,17 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     languageOptions: {
       parser: tseslint.parser,
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // 方法的参数始终是双变的，而函数属性的参数是逆变的，这样设置防止函数窄接口参数类型错误
+      '@typescript-eslint/method-signature-style': [ 'error', 'property' ]
     }
   },
 ])

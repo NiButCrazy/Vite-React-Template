b3aced2237cd8514a3781379a394e2286b620fe5
NiButCrazy <3104955349@qq.com>
2026-05-02 10:06:29 +0800

    fix: base 路径改为动态获取，不会造成无限套娃

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router'
 import { App, Home } from '@renderer/pages'
 
-// 相当于应用创建的是一个相对路径，无需关注前面的路径是什么
-const base = window.location.pathname
+// 获取到 vite 基础 base 路径，当然生产环境也能使用
+const base = import.meta.env.BASE_URL;
 
 const router = createBrowserRouter([
   {

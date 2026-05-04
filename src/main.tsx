dba1d32bb9fad7c818df812e6b675e880d8192c9
NiButCrazy <3104955349@qq.com>
2026-05-04 14:22:14 +0800

    chore: 移除不必要的 .idea 配置文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import '@unocss/reset/normalize.css'
-import '@shared/styles/global.less'
-import 'virtual:uno.css'
+import '@unocss/reset/normalize.css';
+import '@shared/styles/global.less';
+import 'virtual:uno.css';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './router';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={ router } />
   </StrictMode>
-)
+);
